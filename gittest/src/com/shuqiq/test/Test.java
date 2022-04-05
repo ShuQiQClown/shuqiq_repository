577a51164af218fc2f2df16b6163350e706481be
shuqiq <qq1525529043@163.com>
2022-04-05 16:39:22 +0800

    git test

---
+++ gittest/src/com/shuqiq/test/Test.java
@@ -0,0 +1,15 @@
+package com.shuqiq.test;
+
+/**
+ * @package_name: com.shuqiq.test
+ * @project_name: shuqiq_repository
+ * @description:
+ * @author: SHUQIQ
+ * @email: xxx@xx.com
+ * @date: 2022/4/5 16:37
+ */
+public class Test {
+    public static void main(String[] args) {
+       System.out.println("test holle word");
+    }
+}
